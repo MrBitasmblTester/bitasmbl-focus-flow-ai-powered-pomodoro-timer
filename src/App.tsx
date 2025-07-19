b9b4da46d17c76ce67c81b9d95fda20538178d14
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-07-19 10:34:01 +0400

    Update App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useState } from "react";
+import Login from "./components/Login";
+import Lobby from "./components/Lobby";
+import CoinFlip from "./components/CoinFlip";
+import WalletPanel from "./components/Wallet";
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [tableId, setTableId] = useState(null);
+
+  console;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login onToken={setToken} />} />
+        <Route
+          path="/lobby"
+          element={
+            token ? (
+              <>
+                {/* <WalletPanel token={token} /> */}
+                <Lobby token={token} onTableId={setTableId} />
+              </>
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+          // children={token ? <CoinFlip token={token} /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/games/coinflip"
+          element={
+            token ? (
+              <CoinFlip token={token} tableId={tableId} />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
